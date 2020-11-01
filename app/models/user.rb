815f60c64dbde64bbe1b6ac570c42c9bb64c18f3
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-01 11:58:34 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :genre, presence: true
+  validates :grade, presence: true
+  validates :class_number, presence: true
+  validates :student_number, presence: true
 end
