fdf08f146f983ecacf3f8996cbf976dbc6f16f75
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-02 21:38:20 +0900

    アソシエーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
     belongs_to :user
     belongs_to :room
+    has_many :comments
     has_one_attached :image
 
     validates :content, presence: true, unless: :was_attached?
