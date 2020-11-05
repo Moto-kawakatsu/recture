1a3284c57ae5a3a9265099d8bea7e057f93c96f4
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-05 22:41:25 +0900

    コメント表示の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,10 +8,10 @@ class CommentsController < ApplicationController
   end
 
   def new
-    
-    @comments = Comment.new
-    @messages = Message.all
     @message = Message.find(params[:message_id])
+    @comments = @message.comments.order("created_at DESC")
+    @comment = Comment.new
+    @messages = Message.all
     @room = Room.all
   end
 
@@ -19,12 +19,25 @@ class CommentsController < ApplicationController
      @message = Message.find(params[:message_id])
      @comment = @message.comments.new(comment_params)
     if @comment.save
-      redirect_to root_path
+      redirect_to new_message_comment_path(@message)
+    else
+      redirect_to new_message_comment_path(@message)
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @message = @comment.message
+    if @comment.destroy
+       redirect_to new_message_comment_path(@message)
+    else
+       @comments = @message.comments.order("created_at DESC")
+       render :new
+    end       
   end
     
       private
       def comment_params
-        params.require(:comment).permit(:text).merge(user_id: current_user.id, message_id: params[:message_id], room_id: params[:room_id])
+        params.require(:message).permit(:text).merge(user_id: current_user.id, message_id: params[:message_id])
       end
 end
