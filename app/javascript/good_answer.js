7ce829f79b3f186ae773f679b05db8d33ae85f8d
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-07 21:08:05 +0900

    質問解決ボタンの作成

--- app/javascript/good_answer.js
+++ app/javascript/good_answer.js
@@ -1,4 +1,12 @@
-window.onload = function(){ 
-document.getElementById("text-button").onclick = function() {
-    document.getElementById("text").innerHTML = "Good Answer";
-};};
+window.addEventListener("load", function(){ 
+    const goodAnswerButton = document.getElementsByClassName('good-answer')
+    for (let i = 0; i < goodAnswerButton.length; ++i) { 
+        goodAnswerButton[i].addEventListener("click", function(){
+            if (goodAnswerButton[i].getAttribute("style") == "background-color:rgba(255, 115, 0, 0.432);") {
+                goodAnswerButton[i].removeAttribute("style", "background-color:rgba(255, 115, 0, 0.432);")
+            } else {
+                goodAnswerButton[i].setAttribute("style", "background-color:rgba(255, 115, 0, 0.432);")
+            }
+        })
+    }
+})
