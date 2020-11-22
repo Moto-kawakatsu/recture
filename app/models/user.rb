d0874db09617b77ba9d35c7d274af93abd135b77
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-22 22:06:14 +0900

    userモデルファイルのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-  validates :genre, presence: true
-  validates :grade, presence: true
-  validates :class_number, presence: true
-  validates :student_number, presence: true
 
   has_many :room_users
   has_many :rooms, through: :room_users
@@ -16,6 +10,20 @@ class User < ApplicationRecord
   has_many :comments
   has_many :responses
   has_many :agrees, dependent: :destroy
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i
+  
+with_options presence: true do
+  validates :password, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
+  validates :email
+  validates :name
+  validates :genre
+  validates :grade
+  validates :class_number
+  validates :student_number
+
+end
+
 
   def already_agreed?(comment)
     self.agrees.exists?(comment_id: comment.id)
