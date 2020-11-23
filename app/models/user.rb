7b2ad8b5392623f4ea50032c196b77ed767f06d8
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-23 15:15:49 +0900

    passwordのバリデーションを解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,7 @@ class User < ApplicationRecord
   has_many :responses
   has_many :agrees, dependent: :destroy
   
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i
-  
 with_options presence: true do
-  validates :password, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
   validates :email
   validates :name
   validates :genre
