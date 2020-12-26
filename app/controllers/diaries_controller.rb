c766839303377a0488844f3e4b295b3cbe087c35
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-26 09:36:09 +0900

    diariesコントローラーにメソッドを定義

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,13 +1,25 @@
 class DiariesController < ApplicationController
     def index
+        @user = current_user
+        @diaries = @user.diaries.order("created_at DESC")
     end
 
     def new
+        @user = current_user
+        @diary = Diary.new
     end
 
+    def create
+        @diary = Diary.new(diary_params)
+        if @diary.save
+          redirect_to root_path
+        else
+          redirect_to root_path
+        end
+    end
     private
 
     def diary_params
-        params.require(:diary).permit(:writing).merge(user_id: current_user.id)
+        params.require(:diary).permit(:writing, :user_id).merge(user_id: current_user.id)
     end
 end
