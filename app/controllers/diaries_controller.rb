6dabf7f454bbb78c3035dc909c61a481b18f6b25
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-27 09:37:17 +0900

    diariesコントローラーにメソッドを追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -5,18 +5,41 @@ class DiariesController < ApplicationController
     end
 
     def new
-        @user = current_user
         @diary = Diary.new
+        # @diaries = Diary.all
+        @user = User.find(params[:user_id])
+        @diaries = @user.diaries.order("created_at DESC")
     end
 
     def create
         @diary = Diary.new(diary_params)
         if @diary.save
-          redirect_to root_path
+          redirect_to new_user_diary_path(current_user.id)
         else
-          redirect_to root_path
+          redirect_to new_user_diary_path(current_user.id)
         end
     end
+
+    def show
+        @diary = Diary.new
+        # @diaries = Diary.all
+        @user = User.find(params[:user_id])
+        @diaries = @user.diaries.order("created_at DESC")
+        @diary = Diary.find(params[:id])
+    end
+
+    def destroy
+        @user = User.find(params[:user_id])
+        @diaries = @user.diaries
+        @diaries.each do |diary|
+        if diary.destroy
+           return redirect_to root_path
+        else
+           render :new
+        end
+    end
+    end
+
     private
 
     def diary_params
