aaad8bf4751828e496612cd44049c5574423dfb6
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-29 15:07:22 +0900

    repliesコントローラーの作成

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,24 @@
 class RepliesController < ApplicationController
+    def new
+        # @user = User.find(params[:id])
+        @diary = Diary.find(params[:diary_id])
+        @reply = Reply.new
+        # @diaries = @user.diaries.order("created_at DESC")
+     
+    end
+
+    def create
+        @reply = Reply.new(reply_params)
+        if @reply.save!
+          redirect_to root_path
+        else
+          redirect_to root_path
+        end        
+    end
+
+    private
+
+    def reply_params
+        params.require(:reply).permit(:reply_text, :user_id, :diary_id).merge(user_id: current_user.id, diary_id: @diary.id)
+    end
 end
