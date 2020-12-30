f332e8336019f94794ef6d57e72e159c4ed6d91a
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-30 17:06:20 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,17 @@ Rails.application.routes.draw do
   get 'responses/create' => 'responses#create'
   get 'agrees' => 'agrees#show'
   get 'explanations' => 'explanations#index'
-  get 'tips/story' => 'tips#story'
+  get 'tips/lists' => 'tips#lists'
   get 'tips/questions' => 'tips#questions'
   get 'tips/hours' => 'tips#hours'
   get 'tips/commentnum' => 'tips#commentnum'
   get 'rooms/students' => 'rooms#students'
+  get 'messages/list' => 'messages#list'
+  get 'diaries/count_words' => 'diaries#count_words'
 
   root to: "rooms#index"
   resources :users, only: [:edit, :update] do
-    resources :diaries, only: [:index, :new, :create, :show, :destroy]  end
+    resources :diaries, only: [:index, :new, :create,:destroy]  end
 
   resources :rooms, except: [:show] do
     resources :messages, only: [:index, :new, :create, :destroy]
