9fc582c78d939b9446466d9ff4cdf0e88685228b
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-31 14:56:31 +0900

    repliesコントローラーdestroyメソッドの追加

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,24 +1,31 @@
 class RepliesController < ApplicationController
     def new
-        # @user = User.find(params[:id])
         @diary = Diary.find(params[:diary_id])
         @reply = Reply.new
-        # @diaries = @user.diaries.order("created_at DESC")
-     
     end
 
     def create
+        @diary = Diary.find(params[:diary_id])
         @reply = Reply.new(reply_params)
-        if @reply.save!
-          redirect_to root_path
+        if @reply.save
+          redirect_to new_diary_reply_path(@diary)
         else
-          redirect_to root_path
+          render :new
         end        
+    end
+
+    def destroy
+      @reply = Reply.find(params[:id])
+      if @reply.destroy
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
     end
 
     private
 
     def reply_params
-        params.require(:reply).permit(:reply_text).merge(user_id: current_user.id)
+        params.require(:reply).permit(:reply_text).merge(user_id: current_user.id, diary_id: @diary.id)
     end
 end
