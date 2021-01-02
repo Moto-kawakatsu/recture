e3c6ea788b256beecdd46894219bc8ef06d407f0
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-01-02 19:43:01 +0900

    user.rbにtrackableを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
 
   has_many :room_users
