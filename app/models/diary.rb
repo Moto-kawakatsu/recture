fc5a013763e1ef02408d6276b75b187931007c9c
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-01-08 15:31:08 +0900

    study_contentのvalidationを追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -5,6 +5,7 @@ class Diary < ApplicationRecord
     with_options presence: true do
     validates :writing
     validates :title
+    validates :study_content
     validates :study_time
     end
 end
