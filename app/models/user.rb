14cb9e8dfc903e4e6ab3e059a7b40828b88912c3
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-01-09 17:09:34 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
 
 
   has_many :room_users
