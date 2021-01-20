b850148de986ac757ea2f57283bc535bf6e2535e
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-01-20 21:55:48 +0900

    examsモデルのアソシエーションとvalidationの設定

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,2 +1,10 @@
 class Exam < ApplicationRecord
+    belongs_to :user
+
+    with_options presence: true do
+    validates :term
+    validates :subject
+    validates :score
+    validates :considering
+    end
 end
