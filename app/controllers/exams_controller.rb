bf6f43dc867af4f18366a34a72556fce668ef2c2
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-01-21 10:15:44 +0900

    examsコントローラーの設定

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,2 +1,40 @@
 class ExamsController < ApplicationController
-end
+
+        def new
+            @exam = Exam.new
+            @user = User.find(params[:user_id])
+            @exams_first_mid = Exam.all.where(term:"１学期中間") 
+            @exams_first_end = Exam.all.where(term:"１学期期末")
+            @exams_second_mid = Exam.all.where(term:"２学期中間")
+            @exams_second_mid = Exam.all.where(term:"２学期期末")
+            @exams_third = Exam.all.where(term:"学年末")
+        end
+    
+        def create
+            @user = User.find(params[:user_id])
+            @exam = Exam.new(exam_params)
+            if @exam.save
+              redirect_to new_user_exam_path(@user)
+            else
+              @user = User.find(params[:user_id])
+              @exams = @user.exams.order("created_at DESC")
+              render :new
+            end
+        end
+    
+        def destroy
+            @exam = Exam.find(params[:id])
+            @user = @exam.user
+            if @exam.destroy
+                redirect_to new_user_exam_path(@user)
+            else
+                redirect_to root_path
+            end
+        end
+    
+        private
+    
+        def exam_params
+            params.require(:exam).permit(:term, :subject, :score, :considering, :user_id).merge(user_id: current_user.id)
+        end
+    end
