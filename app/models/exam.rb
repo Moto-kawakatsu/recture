4517b87412f36fb718539262c1ad463e3143d41c
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-01-23 09:02:30 +0900

    エラーメッセージの表示順を変更

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,10 +2,10 @@ class Exam < ApplicationRecord
     belongs_to :user
 
     with_options presence: true do
+    validates :grade_num
     validates :term
     validates :subject
     validates :score
     validates :considering
-    validates :grade_num
     end
 end
