8d18e654328ecae4b8a545b8f1472aff4f5fff4f
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-01-31 10:14:47 +0900

    scoreの入力値の上限と下限を設定

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -5,7 +5,7 @@ class Exam < ApplicationRecord
     validates :grade_num
     validates :term
     validates :subject
-    validates :score
+    validates :score, format: {with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 100}
     validates :considering, length: {maximum: 40 }
     end
 end
