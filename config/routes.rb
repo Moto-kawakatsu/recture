6ddd24dccfb8c8d4b90c45ebf2d477f83cd36134
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-02-02 16:39:49 +0900

    taskのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -51,4 +51,6 @@ Rails.application.routes.draw do
   resources :users do
     resources :exams, only: [:new, :create, :destroy, :show]
   end
+
+  resources :tasks, only: [:index, :create, :destroy]
 end
