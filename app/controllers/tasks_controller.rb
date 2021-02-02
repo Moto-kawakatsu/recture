49a06f21d7d0ce78b7709dd1815ea0989c171b05
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-02-03 00:00:13 +0900

    tasksコントローラーの保存・編集・削除の設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,40 +1,64 @@
 class TasksController < ApplicationController
     def index
-      @tasks = Task.all
+      @room = Room.find(params[:room_id])
+      @tasks = @room.tasks
     end
 
     def new
+        @room = Room.find(params[:room_id])
         @task = Task.new
     end
 
     def create
-        if @task = Task.create(task_params)
-            redirect_to tasks_path
+        @task = Task.new(task_params)
+        @room = Room.find(params[:room_id])
+        if @task.save
+            redirect_to room_tasks_path(@room)
+        else
+            redirect_to room_tasks_path(@room)
+        end
+    end
+
+
+    def edit
+        @room = Room.find(params[:room_id])
+        @task = Task.find(params[:id])
+        # if @task.edit(task_params)
+        #     redirect_to room_tasks_path(@room)
+        # else
+        #     render :new
+        # end
+    end
+    
+    def update
+        @room = Room.find(params[:room_id])
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to room_tasks_path(@room)
         else
             render :new
         end
     end
 
-    def edit
-        @task = Task.find(params[:id])
-    end
-    
-    def update
-        @task = Task.find(params[:id])
-        @task.update(task_params)
-        redirect_to tasks_path
-    end
-
     def destroy
         @task = Task.find(params[:id])
-        @task.destroy
-        redirect_to tasks_path
+        @room = Room.find(params[:room_id])
+        if @task.destroy
+          redirect_to room_tasks_path(@room)
+        else
+            render :new
+        end
     end
 
     private
     
     def task_params
-        params.require(:task).permit(:title)
+        @room = Room.find(params[:room_id])
+        params.require(:task).permit(:title, :room_id).merge(room_id: @room.id)
+    end
+        
+    def exam_params
+        params.require(:exam).permit(:term, :subject, :score, :considering, :grade_num, :user_id).merge(user_id: current_user.id)
     end
 
   end
