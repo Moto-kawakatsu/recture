b869141d7f6d407d8f812f6f75390a8a7558b64e
Moto-kawakatsu <kawakatu1227@gmail.com>
2021-02-13 11:57:55 +0900

    生徒一覧ページで各クラスに所属する生徒数を表示する

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -47,6 +47,31 @@ class MessagesController < ApplicationController
   def list 
     @users = User.all.order("class_number ASC").order("student_number ASC")
     @ids = current_user.diary_ids
+
+    # １学年用
+    @users_num11 = User.all.where(grade:1, class_number: 1)
+    @users_num12 = User.all.where(grade:1, class_number: 2)
+    @users_num13 = User.all.where(grade:1, class_number: 3)
+    @users_num14 = User.all.where(grade:1, class_number: 4)
+    @users_num15 = User.all.where(grade:1, class_number: 5)
+    @users_num16 = User.all.where(grade:1, class_number: 6)
+
+    # 2学年用
+    @users_num21 = User.all.where(grade:2, class_number: 1)
+    @users_num22 = User.all.where(grade:2, class_number: 2)
+    @users_num23 = User.all.where(grade:2, class_number: 3)
+    @users_num24 = User.all.where(grade:2, class_number: 4)
+    @users_num25 = User.all.where(grade:2, class_number: 5)
+    @users_num26 = User.all.where(grade:2, class_number: 6)
+
+    # 2学年用
+    @users_num31 = User.all.where(grade:3, class_number: 1)
+    @users_num32 = User.all.where(grade:3, class_number: 2)
+    @users_num33 = User.all.where(grade:3, class_number: 3)
+    @users_num34 = User.all.where(grade:3, class_number: 4)
+    @users_num35 = User.all.where(grade:3, class_number: 5)
+    @users_num36 = User.all.where(grade:3, class_number: 6)
+
   end
 
   def levelup
